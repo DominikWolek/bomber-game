fc6883b6b76bace826784e6125c3f2f3efbc0b5d
Brozek <rafalbrozek98@gmail.com>
2019-04-27 20:06:56 +0200

    Added some utilities to Player, powerups and simple animation to player

--- Bomberman/Bomb.gd
+++ Bomberman/Bomb.gd
@@ -3,18 +3,17 @@ extends StaticBody2D
 var in_area = []
 var who_planted
 var DMG # odnosnie tego bedzie odpowiednio duzy rozprysk bomby w pionie/poziomie
-var timer = null
 
-func explode():
-	for p in in_area:
+func explode(): #gdyby explode było zrobione na "shape"
+	#ze wzgledu na DMG wielkosc rozprysku
+	for p in in_area: # dla area o wielkosci DMG
 		if (p.has_method("exploded")):
 			p.exploded(who_planted)
 	end()
-	
+
 func end():
-	print("XD")
+	print("b00m")
 	queue_free()
 
 func ready():
-	
 	pass
