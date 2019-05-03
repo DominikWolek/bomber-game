7da15ddfa2f2b6ddb4e67db08dacc3758e7a0f53
thefil9 <thefil9@gmail.com>
2019-05-03 14:28:17 +0200

    lel

--- Bomberman/Board/KinematicBody2D.gd
+++ Bomberman/Board/KinematicBody2D.gd
@@ -6,13 +6,13 @@ var velocity = Vector2()
 
 func get_input():
     velocity = Vector2()
-    if Input.is_action_pressed('right'):
+    if Input.is_action_pressed("ui_right"):
         velocity.x += 1
-    if Input.is_action_pressed('left'):
+    if Input.is_action_pressed("ui_left"):
         velocity.x -= 1
-    if Input.is_action_pressed('down'):
+    if Input.is_action_pressed("ui_down"):
         velocity.y += 1
-    if Input.is_action_pressed('up'):
+    if Input.is_action_pressed("ui_up"):
         velocity.y -= 1
     velocity = velocity.normalized() * speed
 
