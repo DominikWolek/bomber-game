970e7e90b4dbb995a03aa08ccd16d395e56cb013
Bartosz Piasecki <mrxhyper@gmail.com>
2019-05-03 18:31:55 +0200

    Added start

---
+++ Bomberman/GUI/NextButton.gd
@@ -0,0 +1,11 @@
+extends Button
+
+func _ready():
+	pass # Replace with function body.
+
+func _on_NextButton_pressed():
+	var error = get_tree().change_scene( "res://Player.tscn");
+	if error == OK:
+		pass
+	else:
+		print(error);
