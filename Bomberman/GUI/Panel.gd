faa2f5b597ecc3203aef17a51df4f648fad7ed1b
Bartosz Piasecki <mrxhyper@gmail.com>
2019-05-11 23:07:14 +0200

    Add startmenu

--- Bomberman/GUI/Panel.gd
+++ Bomberman/GUI/Panel.gd
@@ -4,12 +4,37 @@ var gameInfo = {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	gameInfo.map = 0
-	gameInfo.P1 = {Name = "P1" , isBot=false}
-	gameInfo.P2 = {Name = "P2" , isBot=false}
-	gameInfo.P3 = {Name = "P3" , isBot=false}
-	gameInfo.P4 = {Name = "P4" , isBot=false}
-
-func _process(delta):
+	gameInfo = get_node("/root/ConfigurationNode").gameInfo
+	
+	_mapLabelChanged()
+	_PlayerChanged("P1")
+	_PlayerChanged("P2")
+	_PlayerChanged("P3")
+	_PlayerChanged("P4")
+	
+	
+func _mapLabelChanged():
 	var lab = get_node("mapLabel")
 	lab.text = str(gameInfo.map + 1)
+	
+func _PlayerChanged(player):
+	var play = get_node(player+"Settings")
+	play.get_node("Name").text = gameInfo[player].Name
+	
+func setBot(player,info):
+	gameInfo[player].isBot = info
+	
+func incMap():
+	gameInfo.map = (gameInfo.map+1)%5
+	_mapLabelChanged()
+func decMap():
+	gameInfo.map = (gameInfo.map-1)%5
+	_mapLabelChanged()
+	
+func changePlayerName(player,name):
+	gameInfo[player].Name = name
+	_PlayerChanged(player)
+	
+
+func _process(delta):
+	pass
