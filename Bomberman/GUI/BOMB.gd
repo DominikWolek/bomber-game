ec62bc0637760d870a97bd497b8879691eae561e
Bartosz Piasecki <mrxhyper@gmail.com>
2019-06-04 19:58:21 +0200

    naprawione w koncu wejscie :)

--- Bomberman/GUI/BOMB.gd
+++ Bomberman/GUI/BOMB.gd
@@ -5,13 +5,23 @@ var code
 
 func _ready():
 	playerID = get_parent().playerID
+	
+	
+#funkcja blokuje sygnal i obsluguje zmiane przycisku
 func _on_BOMB_pressed():
+	#sprawdzenie czy inny przycisk nie jest obslugiwany	
+	if ( get_parent().get_parent().getSignal() == true ):
+		return
+	else:
+		get_parent().get_parent().setSignalOn()
+	#proces obslugi wejscia
 	set_process_input(true)
 	add_user_signal("AnyKeyClicked")
 	yield(self,"AnyKeyClicked")
 	get_node("/root/ConfigurationNode")._change_and_commit(playerID,"bomb",code)
 	get_node("/root/ConfigurationNode").UpdateMoveSet(playerID)
-	
+	get_parent().get_parent().setSignalOff()
+#funkcja nadpisujaca standardowa obsluge wejscia	
 func _input(event):
 	if event is InputEventKey:
 			 code = event.get_scancode()
--- Bomberman/GUI/BackButtonToSettings.gd
+++ Bomberman/GUI/BackButtonToSettings.gd
@@ -3,6 +3,8 @@ extends Button
 func _ready():
 	pass # Replace with function body.
 
+
+#zmiana sceny na ustawienia
 func _on_BackButton_pressed():
 	var error = get_tree().change_scene( "res://GUI/SettingsScene.tscn");
 	if error == OK:
